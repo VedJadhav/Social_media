fa7b99ba3707112c55eaeaeb38ffe5021dce67c9
Ved Jadhav <vedjadhav@Veds-MacBook-Air.local>
2025-04-16 22:23:53 +0530

    Update Docker setup and finalize fullstack integration

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Saisai22@social-media-database-1:5432/socialmedia"
+DATABASE_URL = "postgresql://postgres:Saisai22@database:5432/socialmedia"
 
 
 engine = create_engine(DATABASE_URL)
